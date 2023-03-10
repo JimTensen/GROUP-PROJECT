4e476b7885d4dd2b5d916bae7ece906fbb3d0305
Timothy Jensen <timothy.jensen.13@gmail.com>
2023-03-10 09:29:52 -0500

    card flip animation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography';
 import vaultLogo from "./VaultLogoTransparent.PNG";
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Russo One'
+    ].join(','),
+  },});
 
 function Header() {
     return(
@@ -10,19 +19,31 @@ function Header() {
                 <Link to='/home'>
                     <img className="inline-block h-32 w-32"  src={vaultLogo} alt="Vault Logo"/>
                 </Link>
-                <div className="float-right text-2xl inline-block pt-12 px-10 ">
+                <div className="float-right text-2xl inline-block pt-12 px-10">
                     <Link to='/contact'>
-                        <Button className= "hover:text-black text-bold" size="large" variant="contained">Contact</Button>
+                    <ThemeProvider theme={theme}>
+                        <div>
+                            <Button className= "hover:text-black text-bold" size="large" variant="contained">Contact</Button>
+                        </div>
+                    </ThemeProvider>
                     </Link>
                 </div>
                 <div className="float-right text-2xl inline-block hover:text-white pt-12 px-10 ">
                     <Link to='/vault'> 
-                        <Button className= "hover:text-black text-bold" size="large" variant="contained">Vault</Button>
+                    <ThemeProvider theme={theme}>
+                        <div>
+                            <Button className= "hover:text-black text-bold" size="large" variant="contained">Vault</Button>
+                        </div>
+                    </ThemeProvider>
                     </Link>
                 </div>
                 <div className="float-right text-2xl inline-block hover:text-white pt-12 px-10 ">
                     <Link to='/home'>
-                        <Button className= "hover:text-black text-bold" size="large" variant="contained">Home</Button>
+                    <ThemeProvider theme={theme}>
+                        <div>
+                            <Button className= "hover:text-black text-bold" size="large" variant="contained">Home</Button>
+                        </div>
+                    </ThemeProvider>
                     </Link>
                 </div>
             </nav>
